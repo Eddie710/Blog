a286b1424fe69afecb7ac051dec8cda0acb597a6
Edward <edana530@west-mec.org>
2023-11-03 13:19:49 -0700

    WORKING

--- Frontend/src/componets/Posts.jsx
+++ Frontend/src/componets/Posts.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function NewBlog() {
-  const cloud_name="dgq5ru9fd"
+  const cloud_name = "dgq5ru9fd";
   const [newPost, setNewPost] = useState({
     name: "",
     restaurant: "",
     description: "",
     review: "",
-    imageURL:''
-
+    imageURL: ""
   });
+
   const inputsHandler = (e) => {
     setNewPost((prevNext) => ({
       ...prevNext,
@@ -19,21 +19,20 @@ function NewBlog() {
   };
 
   const cloudHandler = (e) => {
-    const formData = new FormData(); 
-       
-    formData.append('file', e.target.files[0]);
-    formData.append("upload_preset", 'Blogimages');
- 
- 
-  axios.post(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, formData)
-      .then((res) => {      
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
+    formData.append("upload_preset", "Blogimages");
+
+    axios
+      .post(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, formData)
+      .then((res) => {
         const imageurl = res.data.secure_url;
-        newPost.imageURL=imageurl
-          // setImage(imageurl)
- 
-      })      
-  }
- 
+        setNewPost((prevNext) => ({
+          ...prevNext,
+          imageURL: imageurl,
+        }));
+      });
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -42,79 +41,81 @@ function NewBlog() {
       .then((res) => {
         console.log(res.data);
         setNewPost({
-            name: "",
-            restaurant: "",
-            description: "",
-            review: "",
-            imageURL:""
+          name: "",
+          restaurant: "",
+          description: "",
+          review: "",
+          imageURL: "",
         });
       });
   };
-  
 
-// useEffect(() => {}, []);
-return(  
+  return (
     <div className="formContainer">
-        <form onSubmit={onSubmit}>
-            <div>
-                <label htmlFor="blogName">Your Name: </label>
-                <input 
-                type="text"
-                name="name"
-                placeHolder="John Doe" 
-                id="name"
-                value={newPost.name}
-                onChange={inputsHandler}
-                required />
-            </div>
-            <div>
-                <label htmlFor="restaurantName">Restaurant Name: </label>
-                <input 
-                type="text"
-                name="restaurant" 
-                placeHolder="e.g; mcdonalds" 
-                id="restaurant" 
-                value={newPost.restaurant}
-                onChange={inputsHandler}
-                required />
-            </div>
-            <div>
-            <label htmlFor="restaurantDescription">Restaurant Description</label>
-                <textarea 
-                name="description" 
-                id="description" 
-                placeHolder="this restaurant..." 
-                value={newPost.description}
-                onChange={inputsHandler}
-                required />
-            </div>
-            <div>
-                <label htmlFor="finalReview">Final Review</label>
-                <input
-                type="text"
-                name="review" 
-                placeHolder="scale of 1-10 /10" 
-                id="review" 
-                value={newPost.review}
-                onChange={inputsHandler}
-                required />
-            </div>
-            <div>
-              <label htmlFor="imageUrl"> Upload Image</label>
-              <input 
-              type="file" 
-              className="form-control"
-              name="image" 
-              id="image"
-              onChange={cloudHandler}
-              onSubmit={inputsHandler} />
-            </div>
-              <div>
-                <button type="submit" className="valid-button">Post</button>
-            </div>
-        </form>
-    </div>   
-
-);}
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="blogName">Blog Name: </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="blog name"
+            id="name"
+            value={newPost.name}
+            onChange={inputsHandler}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="restaurantName">Restaurant Name: </label>
+          <input
+            type="text"
+            name="restaurant"
+            placeholder="e.g; mcdonalds"
+            id="restaurant"
+            value={newPost.restaurant}
+            onChange={inputsHandler}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="restaurantDescription">Restaurant Description</label>
+          <textarea
+            name="description"
+            id="description"
+            placeholder="this restaurant..."
+            value={newPost.description}
+            onChange={inputsHandler}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="finalReview">Final Review</label>
+          <input
+            type="text"
+            name="review"
+            placeholder="scale of 1-10 /10"
+            id="review"
+            value={newPost.review}
+            onChange={inputsHandler}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="imageUrl"> Upload Image</label>
+          <input
+            type="file"
+            className="form-control"
+            name="image"
+            id="image"
+            onChange={cloudHandler}
+          />
+        </div>
+        <div>
+          <button type="submit" className="valid-button">Post</button>
+        </div>
+      </form>
+    </div>
+  );
+}
 
 export default NewBlog;
